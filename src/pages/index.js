3f50420c9f74ac8e988ccf6ff51362820aca5232
clarice_r <claricerdallorto@gmail.com>
2022-02-07 15:54:45 +0000

    Basic hero styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design and code React Apps</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sequi veritatis dignissimos. Quia, voluptatibus ducimus?</p>
-    <Link to="/page-2/">Watch the video</Link>
+    <section className='Hero'>
+      <div className='HeroGroup'>
+        <h1>Learn to design and code React Apps</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sequi veritatis dignissimos. Quia, voluptatibus ducimus?</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </section>
+    
   </div>
 )
 
