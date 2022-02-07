e4d83fc8bd2b098311b8259c09982df541e42cab
clarice_r <claricerdallorto@gmail.com>
2022-02-07 11:34:18 +0000

    watch video button text link

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Learn to design and code React Apps</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sequi veritatis dignissimos. Quia, voluptatibus ducimus?</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch the video</Link>
   </div>
 )
 
